23aeed0314840b73d7c55cdb82bfe95e119039a1
Akshay Prajapati <akshay.prajapati@appunik.com>
2022-01-05 17:22:32 +0530

    env yrl changes

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,5 +1,6 @@
 export default function checkEnvironment(): string {
-  const envUrl = 'http://localhost:3000';
+  const envUrl = 'https://demo-trello-zbzm2.ondigitalocean.app';
+  // 'http://localhost:3000';
   // process.env.NODE_ENV === 'development'
   //   ? 'http://localhost:3000'
   //   : 'https://trello-clone-one.vercel.app';
